355aa51005d56824b1cee9ec264e2c1e4fb1f38d
Pax <kristelfung@gmail.com>
2017-11-08 01:02:46 +0800

    header created

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
-class Button extends Component {
+class Header extends Component {
     render(){
         return (
-            <div>
-                <h1>Header</h1>
+            <div className="header">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-6">
+                            <h1>Dashboard</h1>
+                            <h5>Manage caregivers, job postings, and messages.</h5>
+                        </div>
+                        <div className="col-xs-6 header-image-col">
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default Button;
+export default Header;
