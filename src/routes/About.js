380e4cd0d701fbd77d7fbb34332eb9543c096d0b
Pax <kristelfung@gmail.com>
2017-11-08 00:47:39 +0800

    about page created

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 import Infopage from '../components/Body/Infopage';
+import Footer from '../components/Footer';
 
-const features = {
+const header = {
     title: "About",
     subtitle: "Learn about HealthyLovedOnes and the dream team behind it."
 }
 
 export default () => (
-    <div>About</div>
+    <div>
+        <Navbar/>
+        <Infopage/>
+        <Footer/>
+    </div>
 );
