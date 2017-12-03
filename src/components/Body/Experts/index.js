dec93ec2b2570cbbc9d543b8f60e791eb1cef406
Pax <kristelfung@gmail.com>
2017-12-03 21:07:00 +0800

    finished medical experts landing page

--- src/components/Body/Experts/index.js
+++ src/components/Body/Experts/index.js
@@ -4,22 +4,60 @@ import Header from '../../Header';
 // import photos for medical expert categories, link from CSS file
 import Acupuncture from '../../../images/medicalexperts/acupuncture.jpg';
 import Chiropractic from '../../../images/medicalexperts/chiropractic.jpg';
+import Therapist from '../../../images/medicalexperts/therapist.jpg';
+import SpecialNeeds from '../../../images/medicalexperts/specialneeds.jpg';
+import Psychiatry from '../../../images/medicalexperts/psychiatry.jpg';
+import Psychology from '../../../images/medicalexperts/psychology.jpg';
+import MentalHealth from '../../../images/medicalexperts/health.jpg';
+import Alternative from '../../../images/medicalexperts/alternative.jpg';
 
 class Experts extends Component {
     render() {
         const experts = {
             title: "Medical Experts",
-            subtitle: "Access a network of Medical Experts who are a part of the HLO community."
+            subtitle: "Access a network of Medical Experts who are a part of the HLO community.",
         }
 
         const categories = [
             {
                 image: Acupuncture,             
-                name: "Chinese Medicine/Acupuncture"
+                name: "Chinese Medicine/Acupuncture",
+                link: "medicalexperts/chinese"
             },
             {
                 image: Chiropractic,
-                name: "Chiropractic/Osteopath"
+                name: "Chiropractic/Osteopath",
+                link: "medicalexperts/chiropractic"
+            },
+            {
+                image: Therapist,
+                name: "Therapist",
+                link: "medicalexperts/therapist"
+            },
+            {
+                image: SpecialNeeds,
+                name: "Special Needs",
+                link: "medicalexperts/specialneeds"
+            },
+            {
+                image: Psychiatry,
+                name: "Psychiatry",
+                link: "medicalexperts/psychiatry"
+            },
+            {
+                image: Psychology,
+                name: "Psychology",
+                link: "medicalexperts/psychology"
+            },
+            {
+                image: MentalHealth,
+                name: "Mental Health",
+                link: "medicalexperts/mentalhealth"
+            },
+            {
+                image: Alternative,
+                name: "Alternative Medicine",
+                link: "medicalexperts/alternative"
             }
         ]
 
@@ -31,8 +69,10 @@ class Experts extends Component {
                         {categories.map(function(e){
                             return (
                                 <div className="col-sm-6">
-                                    <img src={e.image} />
-                                    <div className="caption">{e.name}</div>
+                                    <a href={e.link}>
+                                        <img src={e.image} />
+                                        <div className="caption">{e.name}</div>
+                                    </a>
                                 </div>
                             )
                         })}
